63a8a01e39d8258a54eeb588fd1a5fa595a39c18
Darren <you@example.comdarren@kandekore.net>
2023-07-04 16:19:05 +0100

    fetch api

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React, { Component } from "react";
 import Cardlist from "./Cardlist";
 import { robots } from "./robots";
 import Searchbox from "./Searchbox";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ""
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
   }
 
   onSearchChange = (event) => {
